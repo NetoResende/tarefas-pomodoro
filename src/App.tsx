af98c6147cb43316ca19ca1771a75751f1710308
netoresende <raimundogomes826@gmail.com>
2026-06-15 16:25:29 -0300

    commit inicial

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+export function App() {
+
+  return (
+   <>
+    <h1>Olá Mundo! (do App)</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore laboriosam nam assumenda, ipsam odio fugiat aperiam eum ut voluptas at quasi repellat ipsum culpa animi odit quod incidunt molestiae ea.</p>
+   </>
+  )
+}
+
+
