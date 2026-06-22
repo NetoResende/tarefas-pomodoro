70a224cf891d293a0687ef5390ddcbc53134640b
netoresende <raimundogomes826@gmail.com>
2026-06-22 10:13:24 -0300

    Adicionando novas atualizações

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
+import { Heading } from "./component/Heading";
+import "./styles/Theme.css";
+import "./styles/Global.css";
+import { TimerIcon } from "lucide-react";
+
+
 export function App() {
-
   return (
-   <>
-    <h1>Olá Mundo! (do App)</h1>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore laboriosam nam assumenda, ipsam odio fugiat aperiam eum ut voluptas at quasi repellat ipsum culpa animi odit quod incidunt molestiae ea.</p>
-   </>
-  )
-}
-
-
+    <>
+      <Heading>
+        Tarefas!
+        <TimerIcon/>
+      </Heading>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore
+        laboriosam nam assumenda, ipsam odio fugiat aperiam eum ut voluptas at
+        quasi repellat ipsum culpa animi odit quod incidunt molestiae ea.
+      </p>
+    </>
+  );
+};
