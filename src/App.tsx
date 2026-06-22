ad2086efca4a21fdd59f5287cb1efcdf35c844f9
netoresende <raimundogomes826@gmail.com>
2026-06-22 11:04:24 -0300

    Criando a estrutura HTML e CSS do nosso App para cada sessão do site

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,29 @@
-import { Heading } from "./component/Heading";
 import "./styles/Theme.css";
 import "./styles/Global.css";
-import { TimerIcon } from "lucide-react";
-
 
 export function App() {
   return (
-    <>
-      <Heading>
-        Tarefas!
-        <TimerIcon/>
-      </Heading>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore
-        laboriosam nam assumenda, ipsam odio fugiat aperiam eum ut voluptas at
-        quasi repellat ipsum culpa animi odit quod incidunt molestiae ea.
-      </p>
-    </>
+   <>
+     <div className="container">
+      <div className="content">
+        <section>LOGO</section>
+      </div>
+    </div>
+    <div className="container">
+      <div className="content">
+        <section>MENU</section>
+      </div>
+    </div>
+    <div className="container">
+      <div className="content">
+        <section>FORMULARIO</section>
+      </div>
+    </div>
+    <div className="container">
+      <div className="content">
+        <section>FOOTER</section>
+      </div>
+    </div>
+   </>
   );
 };
