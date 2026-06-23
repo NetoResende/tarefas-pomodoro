63049f37e1799603aae3fa3755e6e5616564d4f6
netoresende <raimundogomes826@gmail.com>
2026-06-23 15:58:29 -0300

    Criando o CountDown e ajustando a fonte responsiva

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Logo } from "./components/Logo";
 import { Menu } from "./components/Menu";
 import "./styles/Theme.css";
 import "./styles/Global.css";
+import { CountDown } from "./components/CountDown";
 
 export function App() {
   return (
@@ -12,6 +13,9 @@ export function App() {
       </Container>
       <Container>
         <Menu/>
+      </Container>
+      <Container>
+        <CountDown/>
       </Container>
     </>
   );
