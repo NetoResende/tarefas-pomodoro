9df1e5cd978bf821c0987622f8e9e02a9b6d1e17
netoresende <raimundogomes826@gmail.com>
2026-06-25 10:24:29 -0300

    Criando o componente Footer

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import { DefaultInput } from "./components/DefaultInput";
 import { Cycles } from "./components/Cycles";
 import { DefaultButton } from "./components/DefaultButton";
 import { PlayCircleIcon } from "lucide-react";
+import { Footer } from "./components/Footer";
 import "./styles/Theme.css";
 import "./styles/Global.css";
 
@@ -43,6 +44,10 @@ export function App() {
             <DefaultButton icon={<PlayCircleIcon/>}/>
           </div>
         </form>
+      </Container>
+
+      <Container>
+        <Footer/>
       </Container>
     </>
   );
